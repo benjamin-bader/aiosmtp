87cd0d07f7a776724d04ce73ba84608451f39e39
Ben Bader <ben@bendb.com>
2015-09-27 22:10:33 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 
 install_requires = ['aiohttp']
 
-if sys.version_info >= (3, 4):
-    install_requires = install_requires + ['aiohttp']
-
 tests_require = install_requires + ['nose', 'mocks']
 
 setup(name='aiosmtp',
@@ -13,15 +10,16 @@ setup(name='aiosmtp',
       description=('smtp server for asyncio'),
       long_description='TODO',
       classifiers=[
-          'License :: OSI Approved :: BSD License',
+          'License :: OSI Approved :: Apache Software License',
           'Intended Audience :: Developers',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
           'Topic :: Internet :: WWW/SMTP'],
       author='Ben Bader',
       author_email='pianoben@gmail.com',
-      url='https://github.com/KeepSafe/aiosmtp',
+      url='https://github.com/benjamin-bader/aiosmtp',
       packages=find_packages(),
       install_requires=install_requires,
       test_require=tests_require,
